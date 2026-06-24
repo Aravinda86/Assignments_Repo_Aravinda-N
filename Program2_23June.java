af1c5b14501f013f5e1e5e98be60e9dc9d9311bf
Aravinda N <aravindanaga2010@gmail.com>
2026-06-24 13:43:29 +0530

    23rd Assignment

---
+++ Program2_23June.java
@@ -0,0 +1,37 @@
+package dailyassignments;
+
+public class Program2_23June {
+
+	public static void main(String[] args) {
+
+		//ASCII Value printing A,a,0 & @
+		
+		/*
+		 * char char1 = 'A', char2 = 'a', char3 = '0', char4 = '@'; int value1 = char1,
+		 * value2 = char2, value3 = char3, value4 = char4;
+		 * 
+		 * System.out.println("ASCII value of A : " + value1);
+		 * System.out.println("ASCII value of a : " + value2);
+		 * System.out.println("ASCII value of 0 : " + value3);
+		 * System.out.println("ASCII value of @ : " + value4);
+		 */
+		
+		//Printing Triangle using printing statement
+		
+		/*
+		 * System.out.println("*"); 
+		 * System.out.println("**"); 
+		 * System.out.println("***");
+		 * System.out.println("****"); 
+		 * System.out.println("*****");
+		 */
+		
+		//Type casting double to int
+		
+		double value = 10.75;
+		int castValue = (int) value;
+		System.out.println("Double stored value is:"+ value);
+		System.out.println("Typecasted int value is:"+ castValue);
+	}
+
+}
