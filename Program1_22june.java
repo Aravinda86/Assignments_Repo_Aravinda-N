9b31cc63ed48f4b0ea2bd089af725e82a7bdcf62
Aravinda N <aravindanaga2010@gmail.com>
2026-06-24 10:46:37 +0530

    22June Assignment

---
+++ Program1_22june.java
@@ -0,0 +1,17 @@
+package dailyassignments;
+
+public class Program1_22june {
+
+	public static void main(String[] args) {
+		int studentId = 101;
+		char grade ='A';
+		boolean pass = true;
+		short age = 18;
+		
+		System.out.println("Student ID:"+ studentId);
+		System.out.println("Student Grade:"+ grade);
+		System.out.println("Student Result:"+ pass);
+		System.out.println("Student Age:"+ age);
+	}
+
+}
